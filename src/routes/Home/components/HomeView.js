3637a1f9cdf57e5dfa130209c7a3767a97c4dd6b
zheng521 <1733428550@qq.com>
2017-02-22 22:39:29 +0800

    FEAT:样式的实现

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,14 +1,29 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
 
 export const HomeView = () => (
-  <div>
-    <h4>Welcome!</h4>
-    <img
-      alt='This is a duck, because Redux!'
-      className='duck'
-      src={DuckImage} />
+  <div className="collapse-container">
+  	<div className="collapse-header">
+  		<h3>招聘职位</h3>
+  		<span className="collapse-empty">清空</span>
+  	</div>
+		
+  	<div className="collapse-main">
+  		<div className="collapse-main-header">
+  			<span className="collapse-check"></span>
+  			<h3>工程研发部门</h3>
+  			<span className="collapse-number">22</span>
+  		</div>
+  		<ul>
+  			<li className="collapse-li">
+  				<span className="collapse-li-check"></span>
+  				<h3>mac 开发工程师</h3>
+  				<span className="collapse-li-number">4</span>
+  			</li>
+  		</ul>
+  	</div>
+
+
   </div>
 )
 
